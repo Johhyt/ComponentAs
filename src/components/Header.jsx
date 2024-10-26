b3e0af1939e4c99befbd38f2af12120d1a540659
johhyt <t2yrjo00@students.oamk.fi>
2024-10-26 17:30:30 +0300

    Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import '../styles/Header.css'
+
+function Header(){
+    return(
+        <div>
+            <h1>Welcome to the awesome page!</h1>
+        </div>
+    );
+}
+
+export default Header;
